629847f2aac3a08be25234ed20b0c5acf59e5342
Max G. Faraday <maxgfaraday@6thcolumn.org>
2022-06-10 16:40:24 +0200

    Solidity flycheck added

--- personal/dot_emacs.el
+++ personal/dot_emacs.el
@@ -364,3 +364,15 @@
 ;;-----------------------------------
 
 ;;(setq magithub-debug-mode t)
+(require 'solidity-flycheck)
+(setq solidity-flycheck-solc-checker-active t)
+(setq solidity-flycheck-solium-checker-active t)
+
+(setq flycheck-solidity-solc-addstd-contracts t)
+;;(require 'company-solidity)
+
+(add-hook 'solidity-mode-hook
+	  (lambda ()
+	    (set (make-local-variable 'company-backends)
+		 (append '((company-solidity company-capf company-dabbrev-code))
+			 company-backends))))
